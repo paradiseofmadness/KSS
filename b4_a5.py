c38f5837280141d3dbbc8215294eeee5d5f46e7f
highly unskilled <141437346+paradiseofmadness@users.noreply.github.com>
2025-05-29 15:46:04 +0200

    Add files via upload

---
+++ b4_a5.py
@@ -0,0 +1,10 @@
+def S(n : int, m : int) -> int:
+    if n == 0 and m == 0:
+        return 1
+    if m == 0 or n < m:
+        return 0
+    
+    return S(n-1, m-1) + m*S(n-1, m)
+
+if __name__ == '__main__':
+    print(S(5, 3))
